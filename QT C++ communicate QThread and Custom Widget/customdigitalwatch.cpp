76b0f0d4f58a892ee1a4430990d810fdcac0bedd
Burak Hamdi TUFAN <43945916+thecodeprogram@users.noreply.github.com>
2021-04-11 14:06:22 +0300

    Update customdigitalwatch.cpp

--- QT C++ communicate QThread and Custom Widget/customdigitalwatch.cpp
+++ QT C++ communicate QThread and Custom Widget/customdigitalwatch.cpp
@@ -9,17 +9,6 @@ At: 11.04.2021
 #include "ui_customdigitalwatch.h"
 
 
-//1. QT Form class ı olusturuldu. header code dosyaları ve ui oluşmuş oldu
-//2. header dosyasının içerisine TheWatch adında bir public QWidget kalıtımı alan bir class oluşturuldu
-//3. Thewatch classına Q_OBJECT macrosu eklendi ve bu sayede signal slot kullanılabilir hale geldi.
-//4. Header içine olusturulan TheWatch classına public olarak constructor eklendi ve private olarak diğer nesneler tanımladnı
-//5. Tanımlanan nesneler ve constructor metod ilgili header dosyasına bağlı c++ dosyasında initialize edildi
-//6. TheWatch classının constructorı içerisinde label başlatıldı ve hazırlığı yapıldı. burada QHBoxLayout this anahtar sözcüğü ile çağrılmaya dikkat edilmelidir.
-//7. QThread yapısı aynı header içerisine implmente edildi ve Thewatch constructor içerisinde çağrıldı.
-//8. Ana class constructor de thread ve TheWatch widget ları signal ve slotlar ile bağlandı. ve thread çalıştırıldı.
-//9. Ardından thread başlatıldı ve direk olarak threadın,  custom widgetin signaline bağlanarak güncellemesine izin verildi.
-
-
 
 CustomDigitalWatch::CustomDigitalWatch(QWidget *parent) :
     QWidget(parent),
